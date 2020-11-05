e0f1efbc38d1dcf947ef3f7d4360d56d41f6ac80
wanderley9898 <wanderleysilva989@gmail.com>
2020-11-05 17:32:04 -0300

    final

--- src/componets/Button.jsx
+++ src/componets/Button.jsx
@@ -3,7 +3,16 @@ import './Button.css'
 
 const button = props => {
    return(
-      <button className="button">{props.label}</button>
+      <button 
+      onClick={e => props.click && props.click(props.label)} 
+         className={`
+         button
+         ${props.operation ? 'operation' : ''}
+         ${props.double ? 'double' : ''}
+         ${props.triple ? 'triple' : ''}
+      `}>
+         {props.label}
+      </button>
    )
 }
 
---
+++ src/componets/Display.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Display.css'
+
+const display = props => {
+   return(
+      <div className='display'>{props.value}</div>
+   )
+}
+
+export default display
